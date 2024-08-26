6328992375634e8c38bb9aafd6b65f6e66560dd6
Lana <lana@lana.com>
2024-08-26 12:53:28 -0400

    solucion a patrones

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        int i = 1;
+        int cont = 1;
+        int contAux = 1;
+
+        System.out.println("Favor ingrese un numero");
+        number = sc.nextInt();
+        while (i <= number ) {
+
+            if (i % 2 == 0) {
+                System.out.print(".");
+            } else {
+                System.out.print("*");
+            }
+
+            i++;
+        }
+
+        while (cont <= number) {
+            contAux = 1;
+            while (contAux <= number) {
+                if ( contAux == 1 ) {
+                    System.out.println();
+                }
+                if ( cont == 1  || cont == number) {
+                    System.out.print("*");
+                } else if ( contAux == 1  || contAux == number) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+                contAux++;
+            }
+            cont++;
+        }
+    }
+}
